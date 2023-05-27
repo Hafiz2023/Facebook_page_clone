b391ed88e4c9e3c9f182c091fb5fad7da9fba8e1
Hafiz Muhammad Adil <125850998+Hafiz2023@users.noreply.github.com>
2023-05-26 20:22:53 -0700

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-gray-100 h-screen flex items-center justify-center">
+      {/* {lif side} */}
+      <div className="text-2xl w-1/2 px-8">
+        <Image
+          src={"/Images/facebook.svg"}
+          width={300}
+          height={100}
+          alt="FaceBook Logo"
+        />
+        <p className="ml-8 -mt-3">
+          Facebook helps you connect and share with the people in your life.
+        </p>
+      </div>
+      {/* Right side */}
+      <div className="bg-white flex flex-col p-6 rounded-xl w-1/3">
+        <input
+          className="my-2 border border-1 border-gray-100 p-3 rounded-md focus:outline-1 outline-blue-500 "
+          type="text"
+          placeholder="Email address or phone number"
+        />
+        <input
+          className=" my-2 border border-1 border-gray-100 p-3 rounded-md focus:outline-1 outline-blue-500"
+          type="password"
+          placeholder="password"
+        />
+        <button className="bg-blue-500 my-2 py-2 text-lg font-bold text-white rounded-md hover:bg-blue-600">
+          Log in
+        </button>
+        <p className="cursor-pointer text-blue-500 text-sm my-2 text-center hover:underline">
+          Forgotten password?
+        </p>
+        <span className="my-2">
+          <hr />
+        </span>
+        <button className=" bg-green-500 my-2 py-2 px-2 text-lg font-bold text-white rounded-md hover:bg-green-600 max-w-fit mx-auto">
+          {" "}
+          Create new account
+        </button>
+      </div>
+    </div>
+  );
+}
